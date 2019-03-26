058e1ac5a3bc921287252399abe634f65a2383ca
hirozak <shrak0224@gmail.com>
2019-03-26 21:08:43 +0900

    Add tslint config

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import 'normalize.css';
 import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
